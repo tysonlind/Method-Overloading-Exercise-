c172c7df5e71e4368fb18b54af1fcfe0d2ec5001
tyson lind <tysonfloydlind@gmail.com>
2023-08-29 18:01:16 -0600

    completed the method overloading exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,6 +2,31 @@
 {
     public class Program
     {
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static float Add(float x, float y) { 
+            return x + y; 
+        }
+        public static string Add (int x, int y, bool z)
+        {
+            if (z)
+            {
+                string isPlural;
+                if (x + y > 1)
+                {
+                    isPlural = " dollars";
+                }
+                else
+                {
+                    isPlural = " dollar";
+                }
+                return x + y + isPlural;
+            }
+            return (x + y).ToString();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
